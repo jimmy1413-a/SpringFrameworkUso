93af6d479ad6b4928b74c2371d1465b27a0e8b3b
PENE <marleyrozo@gmail.com>
2025-08-21 17:27:40 -0500

    XD

---
+++ src/main/java/com/empresa/entity/Funcionario.java
@@ -0,0 +1,25 @@
+package com.empresa.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name="funcionario")
+@Data
+public class Funcionario {
+	
+	@Id
+	@Column
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String cedula;
+	private String nombre;
+	private String email;
+	
+	
+}
