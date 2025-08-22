33cee26496277e204ff0a224ae00c7d0bce8daf9
S0NSHIN3 <jimmyalejandrobc@ufps.edu.co>
2025-08-22 17:21:41 -0500

    edicion del controlador

--- src/main/java/com/empresa/controller/FuncionarioControlador.java
+++ src/main/java/com/empresa/controller/FuncionarioControlador.java
@@ -1,12 +1,18 @@
 package com.empresa.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
 import com.empresa.entity.Funcionario;
 import com.empresa.service.FuncionarioService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("api/funcionario")
@@ -16,8 +22,32 @@ public class FuncionarioControlador {
 	private FuncionarioService funcionarioService;
 	
 	@PostMapping
-	public Funcionario post(@RequestBody Funcionario request) {
-		return funcionarioService.guardar(request);
+	public ResponseEntity<Map<String,Object>>POST(@Valid @RequestBody Funcionario request, BindingResult bindingResult) {
+		Map<String,Object> r = new HashMap<>();
+		
+		if(bindingResult.hasErrors()) {
+			String error = bindingResult.getFieldError().getDefaultMessage();
+			r.put("mensaje", "Error en la creacion del paciente.");
+			r.put("error", error);
+			r.put("status", HttpStatus.BAD_REQUEST.value());
+			return ResponseEntity.badRequest().body(r);
+		}
+		
+		try {
+			Funcionario creado = funcionarioService.guardar(request);
+			r.put("funcionario", creado);
+			r.put("mensaje", "Funcionario creado correctamente");
+			r.put("status", HttpStatus.OK.value());
+			return ResponseEntity.ok(r);
+			
+			
+	} catch (Exception e) {
+		r.put("mensaje", "Error en la creacion del paciente");
+		r.put("error", e.getMessage());
+		r.put("mensaje", HttpStatus.NOT_FOUND.value());
+		
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(r);
+	}
 	}
 	
 	@GetMapping
